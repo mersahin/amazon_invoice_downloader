cdf66f0490d223b9c5aad1a4eaf905dbdd63fc46
mehmet erşahin <m@ersah.in>
2023-03-19 22:06:12 +0100

    Update amazon_invoice_downloader.user.js

--- amazon_invoice_downloader.user.js
+++ amazon_invoice_downloader.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name         amazon fatura indirici
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.amazon.*
+// @match        https://www.amazon.de/-/*/your-orders*
+// @match        https://www.amazon.com/-/*/your-orders*
+// @match        https://www.amazon.de/-/*/*/your-account/*
+// @match        https://www.amazon.com/-/*/*/your-account/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.de
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @require      https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js
